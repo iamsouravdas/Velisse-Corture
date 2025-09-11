f6bd1a6787430f9123ae954c52da92af31c5e8ff
hp <hp@SOURAV>
2025-09-11 12:25:26 +0530

    Deleted non required code

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,11 @@
-import axios from 'axios'
 import './App.css'
 import RouteNavigator from './routes/RouteNavigator'
-import { useEffect, useState } from 'react'
 
 function App() {
 
-  const[data, setData] = useState([])
-
-  useEffect(()=> {
-
-    axios.get('http://localhost:3030/api/urban-cart/products/getProductList').then((res)=> setData(res.data.data) )
-  }, [])
-
-  console.log(data);
-
-
-
   return (
     <>
-      {/* <Login /> */}
 
-   {
-    data?.map((element: any)=> {
-      return <img src={element.main_image_url} alt="image name"/>
-    })
-   }
       <RouteNavigator />
 
     </>
